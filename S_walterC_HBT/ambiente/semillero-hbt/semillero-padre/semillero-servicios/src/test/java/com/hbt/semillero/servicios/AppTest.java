0511cc1e2723b846789978197f4817eb716950f1
WalterMauricioCuervoBarahona <walter.cuervo@uptc.edu.co>
2019-10-29 08:03:22 -0500

    en semillero servicio se Añadio la dependencia testNG en el pom y un metodo test de palabra invertidas en el AppTest

--- S_walterC_HBT/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ S_walterC_HBT/ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -1,9 +1,44 @@
 package com.hbt.semillero.servicios;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AppTest {
 
 	public void shouldAnswerWithTrue() {
 
 	}
+	
+	private String invertirCadena(String cadena) {
+		String cadenaInvertida = "";
+		for (int x = cadena.length() - 1; x >= 0; x--) {
+			cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+		}
+		return cadenaInvertida;
+	}	
+			
+	@Test
+	public void invertirCadenaTest() {
+		String resultadoEsperado = "aloH";
+		String inversion = invertirCadena("Hola");
+		String inversion2 = invertirCadena("");
+		Assert.assertNotNull(inversion);
+		Assert.assertTrue(!inversion2.isEmpty());
+		Assert.assertEquals(inversion, resultadoEsperado);		
+	}
+	
+	
+	/*
+	@Test
+	public void primeraPrueba() {
+		Long resultadoEsperado = 150L;
+		Long sumando1 = 100L;
+		Long sumando2 = 50L;
+		Long resultado = sumando1+sumando2;
+		Assert.assertEquals(resultado, resultadoEsperado);
+		Long resultadoEsperadoDos = 200L;
+		Assert.assertNotEquals(resultado, resultadoEsperadoDos);
+	}*/
+	
+	
 }
